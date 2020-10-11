14b97bf139002ea5cfdf37aa9a5e78d2b7bbeb06
Jose Leopoldo <josehcls@gmail.com>
2020-10-11 13:45:56 -0300

    Spring Init

---
+++ src/main/java/br/com/fermentis/tccregisterservice/TccRegisterServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fermentis.tccregisterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccRegisterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccRegisterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fermentis/tccregisterservice/TccRegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fermentis.tccregisterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccRegisterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
